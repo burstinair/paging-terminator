89657fefbd81ee6a94918c0c3a862502d23e5631
Burst <burstinair@gmail.com>
2013-07-19 15:14:21 +0800

    支持更多页面

--- content.js
+++ content.js
@@ -1,27 +1,20 @@
 (function (document) {
 
-var each = 
-Array.prototype.each =
-NodeList.prototype.each =
-HTMLCollection.prototype.each =
-function (callback) {
+var each = function (callback) {
     for(var i = 0; i < this.length; ++i) {
         callback(i, this[i]);
     }
 };
-var indexOf = 
-Array.prototype.each =
-NodeList.prototype.each =
-HTMLCollection.prototype.indexOf =
-function (item) {
+var indexOf = function (item) {
     for(var i = 0; i < this.length; ++i) {
         if(item === this[i]) {
             return i;
         }
     }
+    return -1;
 };
 var get_wrap_ancient = function (el) {
-    var res = el.parentElement;
+    var res = el;
     while(res.parentElement.children.length == 1) {
         res = res.parentElement;
     }
@@ -44,6 +37,20 @@ var load_document = function (url, callback) {
 var is_next_btn = function (btn) {
     return btn.tagName.toLowerCase() == 'a' && /^.?(下一页|下页).?$/.test(btn.innerText);
 };
+var get_kind = function (el) {
+    var res = "";
+    var classList = [];
+    each.call(el.classList, function (i, c) {
+        classList.push(c);
+    });
+    if(classList.length > 0) {
+        res += "." + classList.join('.');
+    }
+    if(el.id && el.id != '') {
+        res += '#' + el.id;
+    }
+    return res;
+};
 var get_position = function(el) {
     var path = [];
     var p = el;
@@ -55,18 +62,7 @@ var get_position = function(el) {
         path.push(part);
         p = p.parentElement;
     }
-    var classList = [];
-    each.call(el.classList, function (i, c) {
-        classList.push(c);
-    });
-    var res = path.reverse().join(' ');
-    if(classList.length > 0) {
-        res += "." + classList.join('.');
-    }
-    if(el.id && el.id != '') {
-        res += '#' + el.id;
-    }
-    return res;
+    return path.reverse().join(' ') + get_kind(el);
 };
 var get_el = function(position, document, satisfy) {
     var els = document.querySelectorAll(position);
@@ -75,6 +71,118 @@ var get_el = function(position, document, satisfy) {
             return els[i];
         }
     }
+};
+var get_path = function (el) {
+    var res = [];
+    while(el.tagName.toLowerCase() != 'body') {
+        res.push(el);
+        el = el.parentNode;
+    }
+    res.push(el);
+    return res.reverse();
+};
+var get_size = function (el) {
+    return el.offsetHeight * el.offsetWidth;
+};
+
+var TYPE_LIST = 1;
+var TYPE_SINGLE_PICTURE = 2;
+var TYPE_TEXT = 3;
+var KIND_COUNT_RATE_LIMIT = 0.3;
+var LIST_ITEM_COUNT_LIMIT = 5;
+var MAX_INT = 2147483647;
+var container_results = [];
+var search_container_in_node = function (el) {
+    //TODO: TextNode, Single Picture, etc.
+    //if(el.)
+    var kind_dic = {};
+    var child_count = el.children.length, kind_count = 0;
+    if(child_count >= LIST_ITEM_COUNT_LIMIT) {
+        for(var i = 0; i < child_count; ++i) {
+            var cel = el.children[i];
+            var kind = cel.tagName + get_kind(cel);
+            if(!kind_dic[kind]) {
+                ++kind_count;
+                kind_dic[kind] = true;
+            }
+        }
+        if(kind_count / child_count < KIND_COUNT_RATE_LIMIT) {
+            container_results.push({
+                container: el,
+                type: TYPE_LIST
+            });
+        }
+    }
+    for(var i = 0; i < child_count; ++i) {
+        var res = search_container_in_node(el.children[i]);
+        if(res) {
+            container_results.push(res);
+        }
+    }
+};
+var search_container = function (document, pager) {
+    container_results = [];
+    search_container_in_node(document.querySelector('body'));
+    
+    var res, max_dis = 0, max_size = 0;
+    var pager_path = get_path(pager);
+    for(var i = 0, l = container_results.length; i < l; ++i) {
+        var container_context = container_results[i];
+        var container_path = get_path(container_context.container);
+        var j = 0;
+        while(pager_path[j] == container_path[j] && j < pager_path.length && j < container_path.length) {
+            ++j;
+        }
+        if(j > max_dis || (j == max_dis && get_size(container_context.container) > max_size)) {
+            max_dis = j;
+            max_size = get_size(container_context.container);
+            res = container_context;
+        }
+    }
+    
+    if(res.type == TYPE_LIST) {
+        var kind_dic = {}, main_kind, max_count = 0;
+        for(var i = 0, l = res.container.children.length; i < l; ++i) {
+            var cel = res.container.children[i];
+            var kind = cel.tagName + get_kind(cel);
+            if(!kind_dic[kind]) {
+                kind_dic[kind] = {
+                    tagName: cel.tagName,
+                    classList: cel.classList,
+                    id: cel.id,
+                    count: 0
+                };
+            } else {
+                kind_dic[kind].count++;
+                if(kind_dic[kind].count > max_count) {
+                    max_count = kind_dic[kind].count;
+                    main_kind = kind_dic[kind];
+                }
+            }
+        }
+        res.should_exclude = function (el) {
+            if(el.nodeType != Node.ELEMENT_NODE) {
+                return false;
+            }
+            if(el.tagName != main_kind.tagName) {
+                return true;
+            }
+            if(el.classList.length == 0 && main_kind.classList.length == 0) {
+                return false;
+            }
+            each.call(el.classList, function (i, c) {
+                if(indexOf.apply(main_kind.classList, c) != -1) {
+                    return false;
+                }
+            });
+            return true;
+        };
+    } else {
+        res.should_exclude = function (el) {
+            return false;
+        };
+    }
+    return res;
 };
 
 var current_context, view_container;
@@ -86,14 +194,15 @@ var detect_context = function (document) {
             if(is_next_btn(btn)) {
                 res.next_btn = btn;
                 res.next_btn_position = get_position(btn);
-                res.pager = get_wrap_ancient(res.next_btn);
+                res.pager = get_wrap_ancient(res.next_btn.parentNode);
             }
         });
-        //var next_document = load_document(res.next_url);
-        //next_document
-        res.container = document.querySelector(".picture-list").children[0];
+        var container_context = search_container(document, res.pager);
+        res.should_exclude = container_context.should_exclude;
+        res.container = container_context.container;
         res.container_position = get_position(res.container);
     } else {
+        res.should_exclude = current_context.should_exclude;
         res.next_btn_position = current_context.next_btn_position;
         res.container_position = current_context.container_position;
         res.next_btn = get_el(res.next_btn_position, document, function (btn) {
@@ -102,27 +211,36 @@ var detect_context = function (document) {
         res.container = get_el(res.container_position, document);
     }
     
-    if(res.next_btn == null) {
+    if(!current_context && res.next_btn == null) {
         return null;
     }
-    res.next_url = res.next_btn.attributes['href'].value;
-    res.items = res.container.children;
+    if(res.next_btn) {
+        res.next_url = res.next_btn.attributes['href'].value;
+    }
+    res.items = [];
+    each.call(res.container.childNodes, function (i, el) {
+        if(!res.should_exclude(el)) {
+            res.items.push(el);
+        }
+    });
     return res;
 };
 
 var merge_view = function (view_container, context) {
     each.call(context.items, function (i, el) {
-        view_container.appendChild(el);
+        view_container.appendChild(el.cloneNode(true));
     });
-};
+ };
 
 var loading = false;
 var load_next_page = function () {
-    if(!loading) {
+    if(!loading && current_context && current_context.next_url) {
         loading = true;
         load_document(current_context.next_url, function (next_doc) {
             current_context = detect_context(next_doc);
-            merge_view(view_container, current_context);
+            if(current_context) {
+                merge_view(view_container, current_context);
+            }
             loading = false;
         });
     }
